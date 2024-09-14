a7fa32e65b15b92fff5cfe5a084e03ed44d891b0
S1erro <riviev92@mail.ru>
2024-09-14 13:01:50 +0300

    Updates

--- src/store/Contacts/initialState.ts
+++
@@ -1,6 +0,0 @@
-import {ContactsState, FetchStatus} from "types";
-
-export const initialContactsState: ContactsState = {
-    contacts: [],
-    status: FetchStatus.pending,
-};
--- src/store/VirtualMachines/initialState.ts
+++
@@ -1,6 +0,0 @@
-import {FetchStatus, VirtualMachinesState} from "types";
-
-export const initialVirtualMachinesState: VirtualMachinesState = {
-    virtualMachines: [],
-    status: FetchStatus.pending,
-};
